828fb8c473fd0afb33744e6eebaa379b1740e16b
Michael Crum <micacer48@gmail.com>
2020-12-14 19:50:33 -0700

    Work on profile page

--- hotTakes/TakeTableViewCell.swift
+++ hotTakes/TakeTableViewCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class TakeTableViewCell: UITableViewCell {
     var bodyLabel: UILabel!
+    var profileImage: UIImageView!
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -25,17 +26,30 @@ class TakeTableViewCell: UITableViewCell {
         bodyLabel.numberOfLines = 0
         contentView.addSubview(bodyLabel)
         
-        //contentView.layer.borderColor = UIColor.black.cgColor
+        profileImage = UIImageView()
+        profileImage.backgroundColor = .cyan
+        profileImage.contentMode = .scaleAspectFit
+        profileImage.translatesAutoresizingMaskIntoConstraints = false
+        profileImage.layer.cornerRadius = 25
+        contentView.addSubview(profileImage)
+        
+        //contentView.layer.borderColor = UIColor.lightGray.cgColor
         //contentView.layer.borderWidth = 0.5
         
         setupConstraints()
     }
 
     func setupConstraints() {
+        NSLayoutConstraint.activate([
+            profileImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            profileImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 30),
+            profileImage.widthAnchor.constraint(equalToConstant: 50),
+            profileImage.heightAnchor.constraint(equalToConstant: 50)
+        ])
         
         NSLayoutConstraint.activate([
-            bodyLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 2),
-            bodyLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 30),
+            bodyLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            bodyLabel.leadingAnchor.constraint(equalTo: profileImage.trailingAnchor, constant: 10),
             bodyLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -30)
         ])
     }
